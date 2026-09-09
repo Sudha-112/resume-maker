7f460ff23b0d112144ceccd166f3f90f1afa5cd1
sudha <sudhapalyal81@gmail.com>
2026-09-09 22:23:25 +0530

    create pages in project

---
+++ resume-ai-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import  './index.css'
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to resume maker frontend</h1>
+      <button class="btn btn-primary">Button</button>
+    </>
+  )
+}
+
+export default App
