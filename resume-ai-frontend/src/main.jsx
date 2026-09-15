9b6ebf017df67b6e041c76a2b6f46f2a97aad909
sudha <sudhapalyal81@gmail.com>
2026-09-15 15:02:22 +0530

    chore: setup AuthProvider and ProtectedRoute in main.jsx

--- resume-ai-frontend/src/main.jsx
+++ resume-ai-frontend/src/main.jsx
@@ -8,23 +8,40 @@ import {BrowserRouter , Routes, Route} from "react-router"
 import Services from './pages/Services'
 import Contact from './pages/Contact'
 import GenerateResume from './pages/GenerateResume'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from './context/AuthContext'
+import ProtectedRoute from './routes/ProtectedRoute'
+import { Navigate } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   
+
    <BrowserRouter>
+   <AuthProvider>
    <Toaster />
    <Routes>
 
    <Route  path="/" element={<Root />}>
+        <Route index element={<Navigate to="/home" replace />} />
         <Route path ="home" element={<Home />} />
         <Route path ="about" element={<About />} />
         <Route path = "services" element={<Services />} />
         <Route path = "contact" element={<Contact />} />
-        <Route path = "generate-resume"  element={<GenerateResume />} />
+        <Route path = "login" element={<Login />} />
+        <Route path = "signup" element={<Signup />} />
+        <Route
+          path = "generate-resume"
+          element={
+            <ProtectedRoute>
+              <GenerateResume />
+            </ProtectedRoute>
+          }
+        />
    </Route>
    </Routes>
+   </AuthProvider>
   </BrowserRouter>
 
   </StrictMode>,
